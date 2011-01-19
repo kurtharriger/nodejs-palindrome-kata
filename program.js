b11ca45e350dc448becda43be760ca9964cdac7d
Kurt Harriger <kurtharriger@gmail.com>
2011-01-18 20:31:26 -0700

    test for 2 letters

--- program.js
+++ program.js
@@ -3,8 +3,11 @@ var assert = require('assert');
 
 // returns true if palindrome false otherwise
 var isPalindrome = function(str) {
-    
-  return str.length;
+  if(str.length == 0) return false;
+  if(str.length == 1) return true;
+
+  return str[0] == str[1];
+  
 }
 
 
@@ -12,6 +15,9 @@ var isPalindrome = function(str) {
 assert.ok(!isPalindrome(""), "empty string is not a palindrome");
 // is a single letter considered a palendrome? Not sure but lets go with yes. 
 assert.ok(isPalindrome("a"), "single letter is a palindrome");
+// 2 letters are palindrome if they are the same
+assert.ok(isPalindrome("aa"), "'aa' is a palindrome");
+assert.ok(!isPalindrome("ab"), "'ab' is not a palindrome");
 
 //assert.ok(isPalindrome("pop"), "'pop' is a palindrome");
 //assert.ok(isPalindrome("Ah, Satan sees Natasha"), "'Ah, Satan sees Natasha' is a palindrome");
