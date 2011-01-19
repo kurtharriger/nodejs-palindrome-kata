eae2ca24f46d577f1c69bd26bd8dff60018563b6
Kurt Harriger <kurtharriger@gmail.com>
2011-01-18 20:14:35 -0700

    define method and acceptance tests

---
+++ program.js
@@ -0,0 +1,12 @@
+
+var assert = require('assert');
+
+// returns true if palindrome false otherwise
+var isPalindrome = function(str) {
+    
+
+}
+
+
+assert.ok(isPalindrome("pop"), "'pop' is a palindrome");
+assert.ok(isPalindrome("Ah, Satan sees Natasha"), "'Ah, Satan sees Natasha' is a palindrome");
