0f037dc16a97e7a20ad122e01fddf55e07beed31
Kurt Harriger <kurtharriger@gmail.com>
2011-01-18 21:22:07 -0700

    isAlpha function

--- program.js
+++ program.js
@@ -2,12 +2,18 @@
 var assert = require('assert');
 
 var compare = function(a, b) {
+    //console.log(a + "=" + b);
     return a.toLowerCase() == b.toLowerCase();
+}
+
+var isAlpha = function(c) {
+  return /[a-zA-Z]/.test(c);    
 }
 
 // returns true if palindrome false otherwise
 var isPalindrome = function(str) {
   if(str.length <= 1) return true;
+
 
   if(compare(str[0],str[str.length -1])) {
     return isPalindrome(str.slice(1,-1));  
@@ -38,5 +44,14 @@ assert.ok(!isPalindrome("abc"), "'abc' is not a palindrome");
 
 // case does is not important
 assert.ok(isPalindrome("aA"), "'aA' case should be ignored");
+
+// non-alpha characters are ignored
+//assert.ok(isPalindrome("a,a", "'a,a' punctuation is ignored"));
+assert.ok(!isAlpha(","), "', is not an alpha character");
+assert.ok(isAlpha("A"), "A is alpha");
+assert.ok(isAlpha("Z"), "Z is alpha");
+assert.ok(isAlpha("a"), "a is alpha");
+assert.ok(isAlpha("z"), "z is alpha");
+
 
 //assert.ok(isPalindrome("Ah, Satan sees Natasha"), "'Ah, Satan sees Natasha' is a palindrome");
