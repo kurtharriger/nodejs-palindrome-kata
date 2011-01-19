d95413f55d3561cc94256eb064ee3c03caacf2cd
Kurt Harriger <kurtharriger@gmail.com>
2011-01-18 20:20:39 -0700

    empty string, single letter tests

--- program.js
+++ program.js
@@ -4,9 +4,14 @@ var assert = require('assert');
 // returns true if palindrome false otherwise
 var isPalindrome = function(str) {
     
-
+  return str.length;
 }
 
 
-assert.ok(isPalindrome("pop"), "'pop' is a palindrome");
-assert.ok(isPalindrome("Ah, Satan sees Natasha"), "'Ah, Satan sees Natasha' is a palindrome");
+// is empty string considered palendrome? I'm going to say no.
+assert.ok(!isPalindrome(""), "empty string is not a palindrome");
+// is a single letter considered a palendrome? Not sure but lets go with yes. 
+assert.ok(isPalindrome("a"), "single letter is a palindrome");
+
+//assert.ok(isPalindrome("pop"), "'pop' is a palindrome");
+//assert.ok(isPalindrome("Ah, Satan sees Natasha"), "'Ah, Satan sees Natasha' is a palindrome");
